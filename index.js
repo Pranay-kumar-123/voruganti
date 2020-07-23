c7552300ea0c7e651ca2de464dca92c066ea6c0c
Pranay-kumar-123 <vorugantipranay@gmail.com>
2020-07-23 15:07:50 +0530

    pranay

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 var express = require('express');
+
+// 2. Create Application
 var app = express();
 
-app.get('/', (req,res) => {
-  res.send("hello");
-});
-app.get('/login', (req,res) => {
-    res.send("you are logged in succesfully");
-  });
-app.get('/logout', (req,res) => {
-    res.send("you are logged out succesfully");
-  });
-  app.listen(3000, function(){
-    console.log("Site Running on http://localhost:");
+// 3.  Define Functiosn to run when someone accesses our site
+app.get('/', function(req, res){
+    res.send('Hello World');
+})
+
+// 4. Run Application on a port ==> 65xxx (65K ports) Mostly we use beyone 3000
+var port= process.env.PORT  || 3000;
+
+// Once site is up, function() will be called automatically
+app.listen(port, function(){
+    console.log("Site Running on http://localhost:"+port);
 }); 

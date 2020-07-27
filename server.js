ff64bd608cb0254c0c936c0579c8b30bea986e2b
Pranay-kumar-123 <vorugantipranay@gmail.com>
2020-07-27 21:43:05 +0530

    added all assignments

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname+'/frontend'));
-
 app.get('/', function(req, res){
-  res.sendFile(__dirname+'/frontend/html/clock.html');
+  res.sendFile(__dirname+'/frontend/html/index.html');
 });
 app.get('/text-tags', function(req, res){
   res.sendFile(__dirname+'/frontend/html/texttags.html');
-})
+});
+app.get('/clock', function(req, res){
+  res.sendFile(__dirname+'/frontend/html/clock.html');
+});
 
 var port= process.env.PORT  || 3000;
 app.listen(port, function(){
